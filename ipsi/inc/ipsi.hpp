c5e72174446fef20626fe50962b24401a243f248
ashwini1025 <ashwiniupasani@cmail.carleton.ca>
2020-11-19 12:50:54 -0500

    ipsi header file codding convention fix

--- ipsi/inc/ipsi.hpp
+++ ipsi/inc/ipsi.hpp
@@ -14,17 +14,11 @@
  * applications to invoke service requests to other applications.
  *
  * External API by IPSI-Framework:
- * int ipsiConnectionType(char *appName,char *connectionName);
- * int ipsiRegisterFunction(void (*funPtr)(), char *methodName);
- * int ipsiMethodCall(char *appName,char* methodName);
- * int ipsiListen();
- * void ipsiClose();
- *
- * Design and Developed by:
- * Partho Ghosal -
- * Ashwini Upasani -
- * Deepti -
- * Rajdeep -
+ * int ipsi_connection_type(char *app_name,char *connection_name);
+ * int ipsi_register_function(void (*fun_ptr)(), char *method_name);
+ * int ipsi_method_call(char *app_name,char* method_name);
+ * int ipsi_listen();
+ * void ipsi_close();
  */
 
 #ifndef IPSI_HPP_
@@ -43,30 +37,30 @@
  * Service provider Application should register itself as "Server" and with a unique name, this will allow the application
  * to server any service request and allow service requester application to invoke services with the unique name.
  *
- * @param[in] appName:  Unique Name of the application
+ * @param[in] app_name:  Unique Name of the application
  *
  * @param[in] connectionType: Specify the role of the application either "Caller" or "Server"
  *
  * @return SUCCESS: The application as successfully registered itself with IPSI library
  * @return FAILURE: The application was unable to register with IPSI library , an appropriate error message will be shown on console
  **/
-int ipsiConnectionType(char *appName,char *connectionName);
+int ipsi_connection_type(char *app_name,char *connection_name);
 
 /**
  * @brief This is an External API use to register callback functions for the services offered by the Service Provider.
  *
  * Service Provider should register its callback functions with a unique name. This unique service name can be used
- * by the Service requester to invoke service request by using ipsiMethodCall().
+ * by the Service requester to invoke service request by using ipsi_method_call().
  *
- * @param[in] funPtr():  callback function that offers services
+ * @param[in] fun_ptr():  callback function that offers services
  *
- * @param[in] methodName: Unique name of the callback function that Service Provider
+ * @param[in] method_name: Unique name of the callback function that Service Provider
  * application should provide to identify its services request
  *
  * @return SUCCESS: The function call was able to register the function with IPSI library
  * @return FAILURE: The function call was unable to register the function with IPSI library
  **/
-int ipsiRegisterFunction(void (*funPtr)(), char *methodName);
+int ipsi_register_function(void (*fun_ptr)(), char *method_name);
 
 /**
  * @brief This is an External API That will allow Service Requester to place service
@@ -75,38 +69,38 @@ int ipsiRegisterFunction(void (*funPtr)(), char *methodName);
  * Service Requester should specify the name of the Service provider and the
  * name of the Service that is provided by the Service provider application.
  *
- * @param[in] appName: Name of the Service Provider Application which
+ * @param[in] app_name: Name of the Service Provider Application which
  * is providing Services that Service Requester Application is interested in.
  *
- * @param[in] methodName: Name of the Service that Service Provider
+ * @param[in] method_name: Name of the Service that Service Provider
  * application is providing to its requester application
  *
  * @return  SUCCESS: The function call was able to place request
  * @return  FAILURE: The function call was not able to place the request.
  **/
-int ipsiMethodCall(char *appName,char* methodName);
+int ipsi_method_call(char *app_name,char* method_name);
 
 /**
  * @brief This is an External API that will allow Service Provider to listen for service request
  *
  * Service Provider will listen for service request that are requested by the Service Requester Application.
  * If the service request is valid it will invoke the service that has been registered with the
- * IPSI library with ipsiRegisterFunction() by the Service provider application.
+ * IPSI library with ipsi_register_function() by the Service provider application.
  *
  *\note This function will block the current thread on which it was called until service invocation was performed
  * and then it will resume to block the current thread for listening any other service request.
  *
- * \note To stop listening for any service request , call ipsiClose() API this will free the current thread on which
- * ipsiListen was operating and should exit with SUCCESS.
+ * \note To stop listening for any service request , call ipsi_close() API this will free the current thread on which
+ * ipsi_listen was operating and should exit with SUCCESS.
  *
  * @return  SUCCESS: It was able to exit Successfully
  * @return  FAILURE: It was not able to exit Successfully as it was not able to able to exit from system's IPC properly
  **/
-int ipsiListen();
+int ipsi_listen();
 
 /**
  * This is an External API that will allow Service Provider application to stop listening to service request.
  **/
-void ipsiClose();
+void ipsi_close();
 
 #endif /* IPSI_HPP_ */
